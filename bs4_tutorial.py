f871afcb28030a27cfdeb93e027057b03441ab47
LesiaMak <101565600+LesiaMak@users.noreply.github.com>
2023-07-01 18:36:40 +0400

    download book covers.

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -3,16 +3,18 @@ import urllib3
 from bs4 import BeautifulSoup
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-url = 'https://tululu.org/b1/'
+url = 'https://tululu.org/b9/'
 response = requests.get(url, verify=False, allow_redirects=False)
 response.raise_for_status()
 soup = BeautifulSoup(response.text, 'lxml')
 title_tag = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('h1')
+image_link = soup.find('div', class_='bookimage').find('img')['src']
 title_text = title_tag.text
 splited_text = title_text.split('::')
 title = splited_text[0].rstrip(' \xa0')
 author = splited_text[1].rstrip(' \xa0')
 print('Заголовок: ', title)
 print('Автор: ', author)
+print(image_link)
 
 
--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import urllib3
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filepath
-
+from urllib.parse import urljoin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -18,7 +18,34 @@ def check_for_redirect(url):
 def download_txt(url, filename, folder="books"):
     os.makedirs(os.path.join('D:/Python/books_website/',folder), exist_ok=True)
     filename = sanitize_filepath(os.path.join(folder, filename + '.txt'))
+    response = check_for_redirect(url)
+    with open(filename, 'w') as file:
+        file.write(response.text)
     return filename
+
+def download_image(url, image_link, folder="images"):
+    os.makedirs(os.path.join('D:/Python/books_website/',folder), exist_ok=True)
+    splited_link =image_link.split('/')
+    image_name = splited_link[-1]
+    filename = sanitize_filepath(os.path.join(folder, image_name))
+    response = check_for_redirect(url)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+    return filename
+
+
+
+def get_image_link(url):
+    response = requests.get(url, verify=False, allow_redirects=False)
+    response.raise_for_status()
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'lxml')
+        image_tag = soup.find('div', class_='bookimage').find('img')['src']
+        image_link = urljoin('https://tululu.org/',image_tag)
+        return image_link
+    else:
+        raise requests.HTTPError("Image havent found")
+
 
 def get_title(url):
     response = requests.get(url, verify=False, allow_redirects=False)
@@ -28,7 +55,7 @@ def get_title(url):
         title_tag = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('h1')
         title_text = title_tag.text
         splited_text = title_text.split('::')
-        title = splited_text[0].rstrip(' \xa0')
+        title = splited_text[0].rstrip(' \xa0')        
         return title
     else:
         raise requests.HTTPError("Book havent found")
@@ -36,15 +63,10 @@ def get_title(url):
 for i in range(1, 10):
     try:
         title_url = 'https://tululu.org/b{}/'.format(i)
-        print(title_url)
         txt_url = 'https://tululu.org/txt.php?id={}'.format(i)
-        print(txt_url)
-        title = get_title(title_url)
-        print(title)
-        responce = check_for_redirect(txt_url)
-        filename = download_txt(txt_url, title)
-        with open(filename, 'w') as file:
-            file.write(responce.text)
+        image_link = get_image_link(title_url)
+        print(image_link)
+        image = download_image(title_url, image_link)
     except requests.HTTPError:
         pass
 
