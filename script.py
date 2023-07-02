859fec80f48e42c0632f8e0b14f97b69577991d5
LesiaMak <101565600+LesiaMak@users.noreply.github.com>
2023-07-02 10:42:46 +0400

    parser books id

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filepath
 from urllib.parse import urljoin
+import argparse
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -15,6 +16,7 @@ def check_for_redirect(url):
     else:
         raise requests.HTTPError("Book havent found")
 
+
 def download_txt(url, filename, folder="books"):
     os.makedirs(os.path.join('D:/Python/books_website/',folder), exist_ok=True)
     book_filename = sanitize_filepath(os.path.join(folder, filename + '.txt'))
@@ -22,6 +24,7 @@ def download_txt(url, filename, folder="books"):
     with open(book_filename, 'w') as file:
         file.write(response.text)
     return filename
+
 
 def download_image(url, image_link, folder="images"):
     os.makedirs(os.path.join('D:/Python/books_website/',folder), exist_ok=True)
@@ -34,14 +37,13 @@ def download_image(url, image_link, folder="images"):
     return filename
 
 
-def get_image_link(url):
+def download_comments(url):
     response = requests.get(url, verify=False, allow_redirects=False)
     response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    image_tag = soup.find('div', class_='bookimage').find('img')['src']
-    image_link = urljoin('https://tululu.org/',image_tag)
-    return image_link
-
+    filename = 'books/comments_{}'.format(parse_book_page(url)['Заголовок'])
+    with open(filename, 'w') as file:
+        file.write(parse_book_page(url)['Комментарии'])
+    return response
 
 
 def parse_book_page(url):
@@ -54,40 +56,46 @@ def parse_book_page(url):
         splited_text = title_text.split('::')
         author = splited_text[1].strip(' \xa0')
         title = splited_text[0].rstrip(' \xa0')
+        image_tag = soup.find('div', class_='bookimage').find('img')['src']
+        image_link = urljoin('https://tululu.org/',image_tag)
+        comments = soup.find_all('div', {'class': 'texts'})
+        all_comments = []
+        for comment in comments:
+            comments_text = comment.text
+            splited_comments = comments_text.split(')')
+            actual_comments = splited_comments[-1]
+            all_comments.append(actual_comments)
         book_genre =[] 
         genres = soup.find('span', class_='d_book').find_all('a')
         for genre in genres:
             book_genre.append(genre.text)
         page = {
-            'Заголовок:': title,
-            'Автор: ': author,
-            'Жанр: ': book_genre,
+            'Заголовок': title,
+            'Автор': author,
+            'Жанр': book_genre,
+            'Image link': image_link,
+            'Комментарии': all_comments
             }
-        return print(page)
+        return page
     else:
         raise requests.HTTPError("Book havent found")
-    
 
 
-def get_comments(url):
-    response = requests.get(url, verify=False, allow_redirects=False)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    comments = soup.find_all('div', {'class': 'texts'})
-    for comment in comments:
-        comments_text = comment.text
-        splited_comments = comments_text.split(')')
-        actual_comments = splited_comments[-1]
-        filename = 'books/comments_{}'.format(get_title(url))
-        with open(filename, 'w') as file:
-            file.write(actual_comments)
-    return response
+parser = argparse.ArgumentParser(
+    description = 'Script downloads books')
+parser.add_argument('start_id', help = "Стартовое id", default=1, type=int)
+parser.add_argument('end_id', help = 'Финишное id', default=10, type=int)
+args = parser.parse_args()
 
-for i in range(1, 10):
+for i in range(args.start_id, args.end_id):
     try:
         title_url = 'https://tululu.org/b{}/'.format(i)
         txt_url = 'https://tululu.org/txt.php?id={}'.format(i)
-        page = parse_book_page(title_url)
+        title = parse_book_page(title_url)['Заголовок']
+        author = parse_book_page(title_url)['Автор']
+        print(title)
+        print(author)
+        book = download_txt(txt_url, title)
     except requests.HTTPError:
         pass
 
