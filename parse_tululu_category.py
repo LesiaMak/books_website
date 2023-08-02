8db3efea0e0ada5022b6b34736a8a1ff7b61034b
LesiaMak <101565600+LesiaMak@users.noreply.github.com>
2023-08-02 13:02:38 +0400

    Add new settings

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -26,15 +26,19 @@ def write_json(parsed_book_page):
     books = json.dumps(parsed_book_page, ensure_ascii = False).encode('utf8')
     with open("books.json", "w") as my_file:
         my_file.write(books.decode())
-    
+
+
 
 def main():
     parser = argparse.ArgumentParser(
         description = 'Script downloads books')
     parser.add_argument('--start_page', help = "Стартовая страница", default=1, type=int)
+    parser.add_argument('--end_page', help = 'Финишная страница', default=701, type=int)
+    parser.add_argument('--dest_folder', help = 'Путь к каталогу с картинками, книгами, JSON.', action="store_true", default=False)
+    parser.add_argument('--skip_img', help = 'Не скачивать картинки', action="store_true", default=False)
+    parser.add_argument('--skip_text', help = 'Не скачивать текст', action="store_true", default=False)
     args = parser.parse_args()
-    parser.add_argument('--end_page', help = 'Финишная страница', default=args.start_page + 1, type=int)
-    args = parser.parse_args()
+
     book_pages = []
     for num in range(args.start_page, args.end_page, 1):
         book_ids = parse_book_ids(get_book_page(f'https://tululu.org/l55/{num}'))
@@ -46,11 +50,18 @@ def main():
                 book_title = book_page['title']
                 book_image = book_page['image']
                 book_pages.append(book_page)
-                script.download_txt(text_payload, book_title)
-                script.download_image(book_path, book_image)
+                if args.skip_img:
+                    script.download_txt(text_payload, book_title)
+                elif args.skip_text:
+                    script.download_image(book_path, book_image)
+                else:
+                    script.download_txt(text_payload, book_title)
+                    script.download_image(book_path, book_image)
         except requests.HTTPError:
             print("Книга не найдена. Введите другой id", file=sys.stderr)
     write_json(book_pages)
+    if args.dest_folder:
+        print(os.getcwd())
             
 if __name__=='__main__':
     main()
