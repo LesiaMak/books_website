77059b35ed5cbcbacc262304bf05b191a117878f
LesiaMak <101565600+LesiaMak@users.noreply.github.com>
2023-08-04 11:58:22 +0400

    Get rid of try except

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -32,39 +32,35 @@ def main():
     parser.add_argument('--dest_folder', help = 'Путь к каталогу с картинками, книгами, JSON.', action="store_true", default=False)
     args = parser.parse_args()
 
-    book_pages = []
-    try:
-        for num in range(args.start_page, args.end_page, 1):
-            try:
-                book_links = parse_book_links(parse_tululu_books.get_book_page(f'https://tululu.org/l55/{num}'))
-            except requests.exceptions.ConnectionError:
-                print('Нет связи с сервером', file=sys.stderr)
-                
-                for book_link in book_links:
-                    try:
-                        text_payload = {'id':'{}'.format(book_link.strip('/b'))}
-                        book_path = (f'https://tululu.org{book_link}')
-                        book_page = parse_tululu_books.parse_page(parse_tululu_books.get_book_page(book_path))
-                        book_title = book_page['title']
-                        book_image = book_page['image']
-                        book_pages.append(book_page)
+    book_pages = []                     
+    for num in range(args.start_page, args.end_page, 1):
+        try:
+            book_links = parse_book_links(parse_tululu_books.get_book_page(f'https://tululu.org/l55/{num}'))
+        except requests.exceptions.ConnectionError:
+            print('Нет связи с сервером', file=sys.stderr)
 
-                        if args.skip_img: parse_tululu_books.download_txt(text_payload, book_title) 
-                        else: parse_tululu_books.download_image(book_path, book_image)
-                        if args.skip_text: parse_tululu_books.download_image(book_path, book_image)
-                        else: parse_tululu_books.download_txt(text_payload, book_title)
+            for book_link in book_links:
+                try:
+                    text_payload = {'id':'{}'.format(book_link.strip('/b'))}
+                    book_path = (f'https://tululu.org{book_link}')
+                    book_page = parse_tululu_books.parse_page(parse_tululu_books.get_book_page(book_path))
+                    book_title = book_page['title']
+                    book_image = book_page['image']
+                    book_pages.append(book_page)
 
-                    except requests.HTTPError:
-                        print("Книга не найдена. Введите другой id", file=sys.stderr)
-                    except requests.exceptions.ConnectionError:
-                        print('Нет связи с сервером', file=sys.stderr)
-                        time.sleep(5)
-        write_json(book_pages)
-        if args.dest_folder:
-            print(os.getcwd())
-    except requests.exceptions.ConnectionError:
-        print('Нет связи с сервером', file=sys.stderr)
-        time.sleep(5)
+                    if args.skip_img: parse_tululu_books.download_txt(text_payload, book_title) 
+                    else: parse_tululu_books.download_image(book_path, book_image)
+                    if args.skip_text: parse_tululu_books.download_image(book_path, book_image)
+                    else: parse_tululu_books.download_txt(text_payload, book_title)
+
+                except requests.HTTPError:
+                    print("Книга не найдена. Введите другой id", file=sys.stderr)
+                except requests.exceptions.ConnectionError:
+                    print('Нет связи с сервером', file=sys.stderr)
+                    time.sleep(5)
+    write_json(book_pages)
+    if args.dest_folder:
+        print(os.getcwd())
 
             
 if __name__=='__main__':
